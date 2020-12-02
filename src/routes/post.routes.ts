c67491dfe6bd93224fe9e1684d5a54ccbb9a8fb9
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2020-12-02 19:58:42 -0300

    DELETE OK

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -3,9 +3,9 @@ import { Router } from 'express';
 const postsRouter = Router();
 
 const posts = [
-    { id: 1, title: "Xuxu blablablablablablabla blablablablabla" },
-    { id: 2, title: "post content number 2" },
-    { id: 3, title: "post number 3" }
+    { id: 0, title: "Xuxu blablablablablablabla blablablablabla" },
+    { id: 1, title: "post content number 2" },
+    { id: 2, title: "post number 3" }
 ];
 
 postsRouter.get('/', (request, response) => {
@@ -24,6 +24,27 @@ postsRouter.post('/', (request, response) => {
     posts.push(post);
 
     return response.json(post);
+})
+
+postsRouter.delete('/', (request, response) => {
+
+    const { id } = (request.query as any);
+
+    console.log(`id = ${id}`);
+
+    let index = posts.findIndex(x => x.id == id);
+
+    console.log(`index = ${index}`);
+
+    if (index == -1) {
+        console.log(`Do not exists`);
+        return response.json({ error: 'Do not exists' }).status(404);
+    }
+
+    posts.splice(index, 1)
+    console.log(posts);
+
+    return response.json(posts).status(200);
 })
 
 export default postsRouter;
